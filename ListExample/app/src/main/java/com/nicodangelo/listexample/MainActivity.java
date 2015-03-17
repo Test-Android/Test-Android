8683dbfb481b563ad10f269cb0f17210605add4a
Jake Cox <nicodangelo15@gmail.com>
2015-03-16 20:17:03 -0500

    I finished the first example of List in android

--- ListExample/app/src/main/java/com/nicodangelo/listexample/MainActivity.java
+++ ListExample/app/src/main/java/com/nicodangelo/listexample/MainActivity.java
@@ -1,44 +1,39 @@
 package com.nicodangelo.listexample;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends Activity
 {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        String[] foods = {"Bacon", "Ham", "Tuna", "Candy", "Meatball", "Potato", "grape", "apple", "sandwich", "pear", "chicken", "water", "nuts", "popcorn", "peanut Butter"};
+        ListAdapter nicosAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,foods);
+        ListView nicosListView = (ListView) findViewById(R.id.nicosListView);
+        nicosListView.setAdapter(nicosAdapter);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        nicosListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener()
+                {
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                    {
+                        String food = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(MainActivity.this, food, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
