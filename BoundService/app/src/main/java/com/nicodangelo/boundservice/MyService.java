ac5d895dd9ad5f6240b0e33d39389c70a8299df5
Jake Cox <nicodangelo15@gmail.com>
2015-03-16 19:37:41 -0500

    I did a bit of stuff and made a example on some new things...
    I also started a list view example

---
+++ BoundService/app/src/main/java/com/nicodangelo/boundservice/MyService.java
@@ -0,0 +1,37 @@
+package com.nicodangelo.boundservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Binder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MyService extends Service
+{
+    private final IBinder nicosBinder = new MyLocalBinder();
+
+    public MyService()
+    {
+    }
+
+    public IBinder onBind(Intent intent)
+    {
+        return nicosBinder;
+    }
+
+    public String getCurrentTime()
+    {
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss", Locale.US);
+        return (df.format(new Date()));
+    }
+
+    //I know this doesn't make sense but just go with it:)
+    public class MyLocalBinder extends Binder{
+        MyService getService()
+        {
+            return MyService.this;
+        }
+    }
+}
